214d3faead49228c4cf9cde02a16166d1a34dce3
kagirimichael <michaelkagiri6@gmail.com>
2025-04-29 15:21:21 +0300

    deployment

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 10 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "production" ? "none" : "lax",
+    sameSite: process.env.NODE_ENV == "production" ? "none" : "lax",
     secure: process.env.NODE_ENV !== "development",
 
   });
