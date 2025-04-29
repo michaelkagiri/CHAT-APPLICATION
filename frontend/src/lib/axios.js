40921fe42bd0eb682e003def32cf55ffbb755167
kagirimichael <michaelkagiri6@gmail.com>
2025-04-29 13:26:51 +0300

    front

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,9 @@
-// import axios from "axios";
 
-// export const axiosInstance = axios.create({
-//     baseURL: "http://localhost:5000/api",
-//     withCredentials: true, // Ensure backend supports credentials (cookies)
-// });
 
 
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? `${import.meta.env.SERVER_URL}/api` : "/api",
   withCredentials: true,
 });
