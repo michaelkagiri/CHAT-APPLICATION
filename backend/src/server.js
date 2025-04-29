8d952406dfd404f027155c6f98a108f6cc801c0b
kagirimichael <michaelkagiri6@gmail.com>
2025-04-29 13:07:28 +0300

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,9 +28,9 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  })
+  // app.get("*", (req,res) => {
+  //   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  // })
 }
 const PORT = process.env.PORT;
 
