8fb37cb8f9c5fc2acdfb6a2f95566af6d212b52d
kagirimichael <michaelkagiri6@gmail.com>
2025-04-29 14:40:52 +0300

    front

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
-
-
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.SERVER_URL}/api`,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =  `${import.meta.env.SERVER_URL}/api`;
+const BASE_URL =  `${import.meta.env.VITE_SERVER_URL}/api`;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
